ab3d3f6ce1cb1de1a2ba500f71e211e43e6f432f
Adastephen <adakolestephen09@gmail.com>
2023-12-28 02:49:34 +0100

    tiktakgame

---
+++ tiktak.js
@@ -0,0 +1,10 @@
+// to create html with 3x3 grid element
+// each cell should have a click handler to play the game
+// cell should return either X or O
+// create a win list of 3 cells to win the game
+// if x in three click align with win list return x player wins
+// if o in three click align with win list return o player wins
+// call functions on pages load to initializae game
+// attach click handler to cell element to play games
+
+const grid = document.querySelector('.table-row');
