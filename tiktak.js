371fe6827144942db5d8108face2ac3330e65327
Adastephen <adakolestephen09@gmail.com>
2023-12-31 06:14:59 +0100

    tictac

--- tiktak.js
+++ tiktak.js
@@ -7,7 +7,7 @@
 // call functions on pages load to initializae game
 // attach click handler to cell element to play games
 
-const tableRow = document.querySelectorAll(".table-row");
+let tableData = document.querySelectorAll("td");
 let one = document.querySelector(".one");
 let two = document.querySelector(".two");
 let three = document.querySelector(".three");
@@ -23,8 +23,7 @@ let o = document.createElement('p');
 // assign letters to x and o
 let xLet = 'X';
 let oLet = 'O';
-x.appendChild(xLet);
-o.appendChild(oLet);
+
 x.style.cssText = 'color: black; font-size: 24px;';
 o.style.cssText = 'color: white; font-size: 24px;';
 
@@ -34,32 +33,78 @@ o.textContent = oLet;
 
 // list of all valids cells
 let winList = [
+    [one, two, three],
     [two, five, eight],
     [one, four, seven],
     [three, six, nine],
     [four, five, six],
     [three, five, seven],
-    [one, five, nine]
+    [one, five, nine],
+    [seven, eight, nine]
 ];
 
-const cellData = document.querySelectorAll(".tabledata");
-cellData.forEach((box) => {
-  box.addEventListener("click", () => {
-    if (box.textContent === "") {
-      let currentPlayer = xLet;
-      function switchPlayer() {
-        currentPlayer = currentPlayer === xLet ? oLet : xLet;
-      }
-      box.textContent = currentPlayer;
-      switchPlayer();
-    }
-  });
-});
-table.appendChild(cellData);
+// one.textContent = 'X';
 
+// one.addEventListener('mouseover', () => {
+//    one.style.cssText = 'background-color: black;';
+// });
+
+const board = ['', '', '', '', '', '', '', '', ''];
+
+tableData.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+        cell.style.cssText ='background-color:gray;';
+    });
+    cell.addEventListener('mouseout', () => {
+        cell.style.cssText = 'background-color:white;';
+    });
+    let currentPlayer = xLet;
+    const switchPlayer = () => {
+        currentPlayer = currentPlayer === xLet ? oLet : xLet;
+        
+    }
+    cell.textContent = switchPlayer();
+});
+            // for (let i = 0; i < winList.length; i++) {
+            //     if (winList[i][0] === winList[i][1] && winList[i][1] === winList[i][2] && winList[i][0] === xLet) {
+            //         alert('X wins');
+            //     } else if (winList[i][0] === winList[i][1] && winList[i][1] === winList[i][2] && winList[i][0] === oLet) {
+            //         alert('O wins');
+            //     }
+    //  });
+//     });
+// });
+
+
+
+    // tableRow[i].appendChild(x);
+    // tableRow[i].appendChild(o);
 
 // function checkWin() {
-// const board = ['', '', '', '', '', '', '', '', ''];
+
+// if (board[0] === board[1] && board[1] === board[2] && board[0]!== '') {
+//     return board[0];
+
+// }
+
+
+// const cellData = document.querySelectorAll(".tabledata");
+// cellData.forEach(box => {
+//   box.addEventListener("click", () => {
+//     if (box.textContent === "") {
+//       let currentPlayer = xLet;
+//       function switchPlayer() {
+//         currentPlayer = currentPlayer === xLet ? oLet : xLet;
+//       }
+//       box.textContent = currentPlayer;
+//       switchPlayer();
+//     }
+//   });
+// });\
+// tableRow.appendChild(cellData);
+
+
+
 
 
 
