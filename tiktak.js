3bb3c6f875d02540f7b69d5fd8b94f1fe20bd6a2
Adastephen <adakolestephen09@gmail.com>
2024-01-26 11:08:55 +0100

    tic tac toe

--- tiktak.js
+++ tiktak.js
@@ -4,7 +4,7 @@
 // create a win list of 3 cells to win the game
 // if x in three click align with win list return x player wins
 // if o in three click align with win list return o player wins
-// call functions on pages load to initializae game
+// call functions on pages load to initialize game
 // attach click handler to cell element to play games
 
 let tableData = document.querySelectorAll("td");
@@ -21,14 +21,14 @@ let x = document.createElement('p');
 let o = document.createElement('p');
 
 // assign letters to x and o
-let xLet = 'X';
-let oLet = 'O';
+// let xLet = 'X';
+// let oLet = 'O';
 
-x.style.cssText = 'color: black; font-size: 24px;';
-o.style.cssText = 'color: white; font-size: 24px;';
+// x.style.cssText = 'color: black; font-size: 24px;';
+// o.style.cssText = 'color: white; font-size: 24px;';
 
-x.textContent = xLet;
-o.textContent = oLet;
+x.textContent = 'X';
+o.textContent = 'O';
 
 
 // list of all valids cells
@@ -43,10 +43,8 @@ let winList = [
     [seven, eight, nine]
 ];
 
-const board = ['', '', '', '', '', '', '', '', ''];
-
 // mouse hover color change
-function hovercolor() {
+function hoverColor() {
     tableData.forEach(cell => {
         cell.addEventListener('mouseover', () => {
             cell.style.cssText ='background-color:gray;';
@@ -56,71 +54,106 @@ function hovercolor() {
         });
     });
 };  
-
+hoverColor();
 
 // check win functions
 function checkWin() {
     for (let i = 0; i < winList.length; i++) {
         if (winList[i][0].textContent === winList[i][1].textContent && winList[i][1].textContent === winList[i][2].textContent) {
-            if (winList[i][0].textContent === xLet) {
+            if (winList[i][0].textContent === x) {
             alert('X wins');
             }
-            else if (winList[i][0].textContent === oLet) {
+            else if (winList[i][0].textContent === o) {
                 alert('O wins');
             }
         }
     };
 };
-
+// random number functions that work behind the comuter layer
 function getRandomElement(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex];
-  }
-
-  let myList = [one, two, three, four, five, six, seven, eight, nine];
-  let randomElement = getRandomElement(myList);
-
-let currentPlayer = xLet; // Start with 'X'
-
-function clickHandler() {
-    tableData.forEach(cell => {
-        cell.addEventListener('click', () => {
-            if (cell.textContent === '') {
-                cell.textContent = currentPlayer;
-
-                // Switch to the next player for the next turn
-                currentPlayer = (currentPlayer === xLet) ? oLet : xLet;
-            }
-        });
-    });
 }
 
+let myList = [one, two, three, four, five, six, seven, eight, nine];
+let randomElement = getRandomElement(myList);
 function clickHandlerComputer() {
     tableData.forEach(cell => {
         cell.addEventListener('click', () => {
             if (cell.textContent === '') {
-                cell.textContent = currentPlayer;
+                cell.textContent = 'X';
+                
 
                 // Switch to the next player for the next turn
                 
-               currentPlayer = (currentPlayer === xLet) ? oLet : xLet;
-               // computer switch
+                // currentPlayer = (currentPlayer === xLet) ? oLet : xLet;
+                // computer switch
                 // automatic switch
-                if (randomElement.textContent === '') {
-                randomElement.textContent = currentPlayer;
-                currentPlayer = (currentPlayer === xLet) ? randomElement : xLet;
-                }
+                
             }
+            if (randomElement.textContent === '') {
+                randomElement.textContent = 'O';
+                // currentPlayer = (currentPlayer === xLet) ? randomElement : xLet;
+            }
+            else{
+                cell.textContent === 'X';
+            };
         });
+        checkWin();
     });
 }
-function runsAll() {
-    hovercolor();
-    // clickHandler();
-    clickHandlerComputer();
-    checkWin();
-}
+clickHandlerComputer();
 
-runsAll();
 
-// clickHandlerComputer();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let currentPlayer = xLet; // Start with 'X'
+
+// function clickHandler() {
+//     tableData.forEach(cell => {
+//         cell.addEventListener('click', () => {
+//             if (cell.textContent === '') {
+//                     cell.textContent = 'X';
+
+//                     players = [
+//                         createPlayer(cell.textContent ==='X' ? cell.textContent ==='X' : "Player 1", "X"),
+
+//                         // createPlayer(document.querySelector("#player2").value ? document.querySelector("#player2").value : "Player 2", "O"),
+//                     //   ];
+//                 }
+//                 else {
+//                     cell.textContent = 'O';
+//                 }
+//                 // cell.textContent = currentPlayer;
+
+//                 // Switch to the next player for the next turn
+//                 // currentPlayer = (currentPlayer === xLet) ? oLet : xLet;
+//             })
+//             // checkWin();
+//         });
+//     };
